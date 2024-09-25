2347d65f77f1c1c0d069606bb189060b529aea11
Ahmad saddin <ahmadsaadaldinn@gmail.com>
2024-09-25 11:25:10 +0300

    banner

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import'./components/Footer'
 import Footer from './components/Footer';
-
+import'./components/Footer'
 import './styles/css/style.css';
 import './styles/scss/style.scss';
+import Banner from './components/Banner';
+
+// Import Bootstrap and jQuery
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.min.js';   // Bootstrap JS
+import 'jquery';                              // jQuery
+
 function App() {
   return (
    <div>
-
+    <Banner/>
  <Footer/>
     </div>
   );
