8aecb96dd3036f7cc5599b1c459c2486b7a3a2d4
KHawara <mohamed.alkhawara@yahoo.com>
2024-09-25 10:07:00 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import'./components/Footer'
 import Footer from './components/Footer';
+import './styles/css/style.css';
+import './styles/scss/style.scss';
 
 function App() {
   return (
